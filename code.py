4bbb6e5f86b6357f7bc4bfe45e1f3cb560af14aa
b3vis <grant@bev.is>
2022-05-27 11:47:18 +0100

    Add a delay to startup

--- code.py
+++ code.py
@@ -6,12 +6,19 @@ import usb_hid
 from adafruit_hid.keyboard import Keyboard
 from adafruit_hid.keyboard_layout_us import KeyboardLayoutUS
 from adafruit_hid.keycode import Keycode
+
+led = digitalio.DigitalInOut(board.LED)
+led.direction = digitalio.Direction.OUTPUT
+
+led.value = True
+time.sleep(1)
+led.value = False
+time.sleep(1)
+led.value = True
+time.sleep(5)
  
 keyboard = Keyboard(usb_hid.devices)
 keyboard_layout = KeyboardLayoutUS(keyboard) 
- 
-led = digitalio.DigitalInOut(board.LED)
-led.direction = digitalio.Direction.OUTPUT
  
 while True:
     led.value = False
